5d834979c987ef4e2d1e38a62f82fe598e419b71
TheManthanvaghela <manthanvaghela05@gmail.com>
2024-05-31 11:03:05 +0530

    add register cntroler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
 
 const app = express()
+app.use(bodyParser.json())
 
 app.use(cors({
     origin:  process.env.CORS_ORIGIN,
@@ -10,8 +12,8 @@ app.use(cors({
 }))
 
 
-app.use(express.json({limit:"16kb "}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.json({limit:"50kb "}))
+app.use(express.urlencoded({extended:true, limit:"50kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())   
 
